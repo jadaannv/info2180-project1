e2103dd60c390d59f791e244b7329903fed33650
Jada-Ann Vite <jadaannv@gmail.com>
2023-11-04 20:24:25 -0500

    Javacode updated

--- scripts.js
+++ scripts.js
@@ -1,1 +1,29 @@
 /* Add your JavaScript to this file */
+
+function isEmail(){
+    let email = document.getElementById("email");
+    let button = document.getElementsByClassName("btn")[1];
+    let message = document.querySelector(".message");
+    let userInput = email.value; 
+
+    button.addEventListener("click", function(e){
+        e.preventDefault();
+        
+        if(userInput == ""){
+            message.textContent = "Please enter a vlid email address."
+            return message;
+        }
+
+        else{
+            message.textContent = "Thank you! Your email address" + userInput + "has been added to our mailing list!"
+            return message;
+        }
+       
+
+    }
+    )
+}
+
+    window.onload = isEmail;
+    
+
